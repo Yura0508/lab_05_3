1e711bcae37ecd1bad0a194e1e75d798fd037ae4
Yura <urijkisil2007@gmail.com>
2024-10-01 22:39:17 +0300

    master commit

---
+++ UnitTest3/UnitTest3.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_5_3/lab_5_3.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest3
+{
+	TEST_CLASS(UnitTest3)
+	{
+	public:
+		
+		// Тест для функції k, коли |x| >= 1
+		TEST_METHOD(TestK_GreaterEqual_1)
+		{
+			double x = 1.5;
+			double result = k(x);
+
+			Assert::IsTrue((result - 5.4519) < 0.0001);
+
+		}
+
+		// Тест для функції k, коли |x| < 1
+		TEST_METHOD(TestK_Less_Than_1)
+		{
+			double x = 0.5;
+			double result = k(x);
+
+			// Проста перевірка, чи результат більше нуля
+			Assert::IsTrue(result > 0);
+
+		}
+
+		// Тест на коректне виконання при x = 0
+		TEST_METHOD(TestK_Zero)
+		{
+			double x = 0.0;
+			double result = k(x);	
+			Assert::AreEqual(1.0, result, 0.0001);
+			
+		}
+	};
+}
---
+++ lab_5_3/lab_5_3.cpp
@@ -0,0 +1,61 @@
+﻿// lab_5_3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std; 
+double k(const double x);
+int main()
+{
+	double zp, zk, h;
+	int n;
+	cout << "zp = "; cin >> zp;
+	cout << "zk = "; cin >> zk;
+	cout << "n = "; cin >> n;
+	double dz = (zk - zp) / n;
+	double z = zp;
+	cout << fixed
+	 << "----------------------------------------" << endl;
+	cout << "|" << setw(10) << "z" << "    |"
+	<< setw(16) << "h" << "       |"
+	<< endl;
+	cout << "----------------------------------------" << endl;
+
+
+	while (z <= zk)
+	{
+		h = k(z * z + 1) - k(z * z - 1) + 2 * k(z);
+		
+		z += dz;
+
+		cout << "|" << setw(12) << setprecision(4) << z << "  |"
+			<< setw(20) << setprecision(4) << h << "   |"
+			<< endl;
+	}
+	cout << "----------------------------------------" << endl;
+	return 0;
+
+}
+double k(const double x)
+{
+	if (abs(x) >= 1)
+		return (exp(x) + sin(x)) / (pow (cos(x), 2) + 1);
+	else
+	{
+		double S = 0;
+		int i = 0;
+		double a = 1;
+		S = (1 / exp(x)) * a;
+		do
+		{
+			i++;
+			double R = x / i;
+			a *= R;
+			S += a;
+			
+		} while (i < 5);
+		return S;
+	}
+} 
+
